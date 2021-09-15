f90115c1640a7233e4857d1744eade829a8af58e
sol <safe@vmi509528.contaboserver.net>
2021-09-15 23:34:40 +0200

    temporary capitalization accomodation

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -4472,7 +4472,7 @@ impl Bank {
                 &self.ancestors,
                 Some(self.capitalization()),
             );
-        if total_lamports  != self.capitalization() {
+        if total_lamports  > self.capitalization() {
             datapoint_info!(
                 "capitalization_mismatch",
                 ("slot", self.slot(), i64),
