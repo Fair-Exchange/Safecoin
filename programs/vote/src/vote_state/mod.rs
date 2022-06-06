6b6133a396dc32d3dbb97adaab36830eefd141d3
jeffowy <jjgallow@gmail.com>
2022-06-06 04:00:19 -0600

    Consensus - random voter selection

--- programs/vote/src/vote_state/mod.rs
+++ programs/vote/src/vote_state/mod.rs
@@ -998,6 +998,25 @@ pub fn process_vote<S: std::hash::BuildHasher>(
 
     let mut vote_state = versioned.convert_to_current();
     let authorized_voter = vote_state.get_and_update_authorized_voter(clock.epoch)?;
+
+
+// Filter consensus for a randomly chosen subset of validators
+// Generate random integers from slot hash and pubkeys
+let mut slot_hash_int = ( (slot_hashes[0].1.to_string().chars().nth(0).unwrap() as usize ) % 10 ) as usize;
+let mut mixed_int = ( ( ( (slot_hashes[0].1.to_string().chars().nth(0).unwrap() as usize ) % 9 + 1 ) as usize
+       	 * ( authorized_voter.to_string().chars().last().unwrap() as usize
+       	   + slot_hashes[0].1.to_string().chars().last().unwrap() as usize ) / 10 ) as usize
+	   + authorized_voter.to_string().chars().last().unwrap() as usize
+	   + slot_hashes[0].1.to_string().chars().last().unwrap() as usize ) % 10 as usize;
+
+//  Compare generated integers to determine voter selection.   Given method has a 3/10 chance, plus bootstrap option
+if slot_hash_int > (mixed_int + 1) ||  slot_hash_int < (mixed_int -1) {
+	     if authorized_voter.to_string() != "83E5RMejo6d98FV1EAXTx5t4bvoDMoxE4DboDee3VJsu" {    // Bootstrap validator for early stability
+	     	 return Err(InstructionError::UninitializedAccount);
+              }
+    }
+
+
     verify_authorized_signer(&authorized_voter, signers)?;
 
     vote_state.process_vote(vote, slot_hashes, clock.epoch)?;
