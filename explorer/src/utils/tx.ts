c3f2b7918266ed4d71f1fd87d8ac1e7e4faffefc
jeffowy <jjgallow@gmail.com>
2023-03-10 09:20:21 -0700

    Safecoin keys

--- explorer/src/utils/tx.ts
+++ explorer/src/utils/tx.ts
@@ -186,7 +186,7 @@ export const PROGRAM_INFO_BY_ID: { [address: string]: ProgramInfo } = {
     name: PROGRAM_NAMES.TOKEN,
     deployments: ALL_CLUSTERS,
   },
-  metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s: {
+  metXT2PkoCBucKkvmeQoYDNmncrPXGVCGacwqEHxcM5: {
     name: PROGRAM_NAMES.TOKEN_METADATA,
     deployments: LIVE_CLUSTERS,
   },
